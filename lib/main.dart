7cc9f4534f8e62d515c7715d57ab736a58de5edb
Biju Krishnan <biju.krishnan@gmail.com>
2026-03-30 15:50:52 +0200

    added a new line on main.dart on line 137

--- lib/main.dart
+++ lib/main.dart
@@ -133,6 +133,8 @@ class _MainTabScreenState extends State<MainTabScreen> {
             Text(widget.translate(l10n('Testautomatisierung ist super'))),
             const SizedBox(height: 10),
             Text(widget.translate(l10n('Unsere Experten sind jetzt für Sie da'))),
+            const SizedBox(height: 10),
+            Text(widget.translate(l10n('Wir sind Alles Clara'))),
             const SizedBox(height: 20),
             Text('Demo Active Locale: ${widget.locale.languageCode}', style: const TextStyle(color: Colors.grey)),
           ],
